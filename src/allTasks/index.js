d55e531d76ecdde0a031a46751d142a76192cb89
BenjaminGordonCody <b.gordoncody@gmail.com>
2022-03-01 16:02:16 +0000

    how each task should appear on screen

---
+++ src/allTasks/index.js
@@ -0,0 +1,14 @@
+export const taskDescriptions = {
+  water: {
+    string: "Drink your water!",
+    emoji: "🚰",
+  },
+  goOut: {
+    string: "Have you gone outside today?",
+    emoji: "🍃",
+  },
+  wrestleAShark: {
+    string: "The predator of the deep!",
+    emoji: "🦈",
+  },
+};
