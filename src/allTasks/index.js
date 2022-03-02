a81ca212d97ea92494f7367e2b41560955c382c6
BenjaminGordonCody <b.gordoncody@gmail.com>
2022-03-02 10:51:39 +0000

    more tasks added

--- src/allTasks/index.js
+++ src/allTasks/index.js
@@ -1,15 +1,87 @@
 const taskDescriptions = {
+  deepBreath: {
+    string: "Breathe deeply for 5 mins",
+  },
+  walk: {
+    string: "Have a walk",
+  },
+  herbs: {
+    string: "Pot some herbs",
+  },
+  bake: {
+    string: "Bake",
+  },
+  read: {
+    string: "Read for 5 mins",
+  },
+  hiit: {
+    string: "Do HIIT for 5 mins",
+  },
+  bath: {
+    string: "Have a bath",
+  },
+  phone: {
+    string: "Phone a friend",
+  },
+  doodle: {
+    string: "Doodle",
+  },
+  smoothie: {
+    string: "Make a smoothie",
+  },
   water: {
-    string: "Drink your water!",
-    emoji: "🚰",
+    string: "Drink a glass of water",
   },
-  goOut: {
-    string: "Have you gone outside today?",
-    emoji: "🍃",
+  tea: {
+    string: "have a cup of tea",
   },
-  wrestleAShark: {
-    string: "The predator of the deep!",
-    emoji: "🦈",
+  workout: {
+    string: "Do a workout",
+  },
+  grateful: {
+    string: "Write a list of 5 things you're grateful for",
+  },
+  journal: {
+    string: "Write in your journal",
+  },
+  facemask: {
+    string: "Do a facemask",
+  },
+  music: {
+    string: "Listen to music",
+  },
+  playGame: {
+    string: "Play a game",
+  },
+  springClean: {
+    string: "Do a spring clean",
+  },
+  vegetablePatch: {
+    string: "Work in vegetable patch",
+  },
+  nature: {
+    string: "Get out in nature",
+  },
+  dance: {
+    string: "Dance for 5 mins",
+  },
+  draw: {
+    string: "Draw a picture",
+  },
+  colour: {
+    string: "Colour in a colouring book",
+  },
+  trip: {
+    string: "Plan a trip",
+  },
+  stretch: {
+    string: "Do some stretches",
+  },
+  forestBathe: {
+    string: "Forest bathe",
+  },
+  stargaze: {
+    string: "Stargaze",
   },
 };
 
