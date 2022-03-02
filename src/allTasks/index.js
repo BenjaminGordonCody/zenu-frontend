9111b0b7f974a32c933f73856deefd77e08fe174
BenjaminGordonCody <b.gordoncody@gmail.com>
2022-03-02 12:10:27 +0000

    Expanded list of activities

--- src/allTasks/index.js
+++ src/allTasks/index.js
@@ -1,87 +1,115 @@
 const taskDescriptions = {
   deepBreath: {
     string: "Breathe deeply for 5 mins",
+    emoji: "🫁",
   },
   walk: {
     string: "Have a walk",
+    emoji: "🚶🏽",
   },
   herbs: {
     string: "Pot some herbs",
+    emoji: "🌿",
   },
   bake: {
     string: "Bake",
+    emoji: "🧁",
   },
   read: {
     string: "Read for 5 mins",
+    emoji: "📖",
   },
   hiit: {
     string: "Do HIIT for 5 mins",
+    emoji: "🏋🏽",
   },
   bath: {
     string: "Have a bath",
+    emoji: "🛀🏽",
   },
   phone: {
     string: "Phone a friend",
+    emoji: "📞",
   },
   doodle: {
     string: "Doodle",
+    emoji: "✏️",
   },
   smoothie: {
     string: "Make a smoothie",
+    emoji: "🍹",
   },
   water: {
     string: "Drink a glass of water",
+    emoji: "🚰",
   },
   tea: {
-    string: "have a cup of tea",
+    string: "Have a cup of tea",
+    emoji: "🍵",
   },
   workout: {
     string: "Do a workout",
+    emoji: "💪🏽",
   },
   grateful: {
     string: "Write a list of 5 things you're grateful for",
+    emoji: "🥲",
   },
   journal: {
     string: "Write in your journal",
+    emoji: "✍🏽",
   },
   facemask: {
     string: "Do a facemask",
+    emoji: "💅🏽",
   },
   music: {
     string: "Listen to music",
+    emoji: "🎶",
   },
   playGame: {
     string: "Play a game",
+    emoji: "🎮",
   },
   springClean: {
     string: "Do a spring clean",
+    emoji: "🧹",
   },
   vegetablePatch: {
     string: "Work in vegetable patch",
+    emoji: "🍆",
   },
   nature: {
     string: "Get out in nature",
+    emoji: "🌳",
   },
   dance: {
     string: "Dance for 5 mins",
+    emoji: "💃🏽",
   },
   draw: {
     string: "Draw a picture",
+    emoji: "🖼️",
   },
   colour: {
     string: "Colour in a colouring book",
+    emoji: "🖍️",
   },
   trip: {
     string: "Plan a trip",
+    emoji: "🧳",
   },
   stretch: {
     string: "Do some stretches",
+    emoji: "🧘🏽",
   },
   forestBathe: {
     string: "Forest bathe",
+    emoji: "🌲",
   },
   stargaze: {
     string: "Stargaze",
+    emoji: "✨",
   },
 };
 
