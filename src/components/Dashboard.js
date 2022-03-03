400e73682e9d165f6d3ef8092d2cb839eda6c530
BenjaminGWilson <benjamin.g.wilson@gmail.com>
2022-03-03 10:44:24 +0000

    user's task completion stored in state

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,40 @@
+import { useState } from "react";
 import { Task } from "./Task";
 
 const taskDescriptions = require("../allTasks");
 
+const blankDailyTaskRecord = () => {
+  let tempObj = {};
+  Object.keys(taskDescriptions).forEach((task) => {
+    tempObj[task] = false;
+  });
+  return tempObj;
+};
 export const Dashboard = ({ user, setPage }) => {
+  //states
+  const [dailyTaskRecord, setDailyTaskRecord] = useState(
+    blankDailyTaskRecord()
+  );
+
+  //update util
+  const updateDailyTaskRecord = (key, value) => {
+    const temp = { ...dailyTaskRecord };
+    temp[key] = value;
+    setDailyTaskRecord(temp);
+  };
+
   return (
     <div id="dashboard" className="page">
       {Object.keys(taskDescriptions).map((task, index) => {
-        return <Task task={taskDescriptions[task]} index={index} />;
+        return (
+          <Task
+            task={taskDescriptions[task]}
+            taskTag={task}
+            index={index}
+            dailyTaskRecord={dailyTaskRecord}
+            updateDailyTaskRecord={updateDailyTaskRecord}
+          />
+        );
       })}
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,20 @@
 import { useState } from "react";
 
-export const Task = ({ task, index }) => {
+export const Task = ({
+  task,
+  index,
+  dailyTaskRecord,
+  updateDailyTaskRecord,
+  taskTag,
+}) => {
   const [taskCompletion, setTaskCompletion] = useState(false);
   return (
     <div className="task" key={index}>
       <div
         className={"bigEmoji" + taskCompletion}
         onClick={() => {
-          setTaskCompletion(!taskCompletion);
+          setTaskCompletion(!taskCompletion); //component's state, for styling
+          updateDailyTaskRecord(taskTag, taskCompletion);
         }}
       >
         {task.emoji}
