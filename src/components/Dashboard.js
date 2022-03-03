0ccdfd088089d5da4405acf00a677d32335c504a
BenjaminGWilson <benjamin.g.wilson@gmail.com>
2022-03-03 14:22:46 +0000

    "submit" button produces updated taskTally obj

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Task } from "./Task";
-
 const taskDescriptions = require("../allTasks");
 
 const blankDailyTaskRecord = () => {
@@ -10,32 +9,56 @@ const blankDailyTaskRecord = () => {
   });
   return tempObj;
 };
+
 export const Dashboard = ({ user, setPage }) => {
   //states
   const [dailyTaskRecord, setDailyTaskRecord] = useState(
     blankDailyTaskRecord()
   );
+  const [totalTaskRecord, setTotalTaskRecord] = useState(
+    JSON.parse(user.taskTally)
+  );
 
-  //update util
+  //update utils
   const updateDailyTaskRecord = (key, value) => {
     const temp = { ...dailyTaskRecord };
     temp[key] = value;
     setDailyTaskRecord(temp);
   };
 
+  const newTaskRecordForDatabase = () => {
+    let newTaskRecord = { ...totalTaskRecord };
+    Object.keys(dailyTaskRecord).forEach((task) => {
+      if (dailyTaskRecord[task] && newTaskRecord.hasOwnProperty(task)) {
+        newTaskRecord[task] += 1;
+      } else if (dailyTaskRecord[task]) {
+        newTaskRecord[task] = 1;
+      }
+    });
+    console.log(newTaskRecord);
+
+    // THIS IS WHERE TO ADD THE LINK TO THE DATABASE, PUT FUNC IN UTILS
+  };
+
   return (
     <div id="dashboard" className="page">
-      {Object.keys(taskDescriptions).map((task, index) => {
-        return (
-          <Task
-            task={taskDescriptions[task]}
-            taskTag={task}
-            index={index}
-            dailyTaskRecord={dailyTaskRecord}
-            updateDailyTaskRecord={updateDailyTaskRecord}
-          />
-        );
-      })}
+      <div id="dashboardTaskContainer">
+        <button id="submitDailyTasks" onClick={newTaskRecordForDatabase}>
+          submit
+        </button>
+        {Object.keys(taskDescriptions).map((task, index) => {
+          return (
+            <Task
+              task={taskDescriptions[task]}
+              taskTag={task}
+              index={index}
+              dailyTaskRecord={dailyTaskRecord}
+              updateDailyTaskRecord={updateDailyTaskRecord}
+            />
+          );
+        })}
+      </div>
+      <div id="dashboardTallyContainer"></div>
     </div>
   );
 };
